72d42bd3edf92bef12e938a0081e684433c2f87c
Jorge Leandro <jleandro@vision.ime.usp.br>
2011-09-05 23:37:17 -0300

    Alternativa para criar tabelas com Hibernate

---
+++ src/sigp/src/CriaTabela.java
@@ -0,0 +1,29 @@
+package sigp.src;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+public class CriaTabela {
+    public static void main(String[] args) {
+    	
+	Configuration configuration = new Configuration();	
+	configuration.addAnnotatedClass(Disciplina.class);
+	configuration.addAnnotatedClass(Grupo.class);
+	configuration.addAnnotatedClass(Membro.class);
+	configuration.addAnnotatedClass(Publicacao.class);
+	configuration.addAnnotatedClass(Projeto.class);
+	configuration.addAnnotatedClass(Veiculo.class);
+	
+	configuration.configure();
+	
+	SessionFactory factory = configuration.buildSessionFactory();
+	Session session = factory.openSession();
+	
+	Transaction tx = session.beginTransaction();
+	new SchemaExport(configuration).create(true, true);
+	tx.commit();
+    }
+}
