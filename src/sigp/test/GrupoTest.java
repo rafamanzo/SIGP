e323d4a25e0a35a2cd553bb88b9aaea9200de816
Jorge Leandro <jleandro@vision.ime.usp.br>
2011-09-09 13:49:54 -0300

    GrupoTest: correcao - usando objetos Mock

--- src/sigp/test/GrupoTest.java
+++ src/sigp/test/GrupoTest.java
@@ -1,6 +1,8 @@
 package sigp.test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +13,14 @@ import org.junit.Test;
 
 import sigp.src.Grupo;
 import sigp.src.LinhaPesquisa;
+import sigp.src.Projeto;
 
 public class GrupoTest {
     
     private Grupo grupo;
     private List<LinhaPesquisa> pesquisas;
+    private List<Projeto> projetos1;
+    private List<Projeto> projetos2;
 
     @Before
     public void setUp() throws Exception {
@@ -34,23 +39,68 @@ public class GrupoTest {
     
     @Test
     public void deveriaDevolverLinhasPesquisa() {
-	assertSame("Deveria devolver a primeira linha de pesquisa", "Sistemas",
+	assertSame("Deveria devolver o nome da primeira linha de pesquisa", "Métodos Ágeis",
 		grupo.getPesquisas().get(0).getName());
-	assertSame("Deveria devolver a segunda linha de pesquisa",
-		"Bancos de Dados", grupo.getPesquisas().get(1)
-			.getName());
+	assertSame("Deveria devolver o nome da segunda linha de pesquisa",
+		"Software Livre", grupo.getPesquisas().get(1).getName());
 	assertArrayEquals(pesquisas.toArray(), grupo.getPesquisas().toArray());
     }
     
+    @Test
+    public void deveriaDevolverProjetosDeCadaLinhaDePesquisa() {
+	assertSame("Deveria devolver o nome do primeiro projeto da primeira linha de pesquisa", "Métodos de Otimização",
+		grupo.getPesquisas().get(0).getProjetos().get(0).getDescricao());
+	assertSame("Deveria devolver o nome do segundo projeto da primeira linha de pesquisa", "Uso eficaz de Métricas",
+		grupo.getPesquisas().get(0).getProjetos().get(1).getDescricao());
+	
+	
+	assertSame("Deveria devolver o nome do primerio projeto da segunda linha de pesquisa",
+		"Achmus", grupo.getPesquisas().get(1).getProjetos().get(0).getDescricao());
+	assertSame("Deveria devolver o nome do segundo projeto da segunda linha de pesquisa",
+		"Arquimedes", grupo.getPesquisas().get(1).getProjetos().get(1).getDescricao());	
+	
+	assertArrayEquals(projetos1.toArray(), grupo.getPesquisas().get(0).getProjetos().toArray());
+	assertArrayEquals(projetos2.toArray(), grupo.getPesquisas().get(1).getProjetos().toArray());
+    }
+    
     private Grupo criarGrupo(){
-	grupo = new Grupo();
-	grupo.setNome("Grupo de Sistemas de Software");
+	
+	Projeto projeto11 = mock(Projeto.class);
+	Projeto projeto12 = mock(Projeto.class);	
+	Projeto projeto21 = mock(Projeto.class);
+	Projeto projeto22 = mock(Projeto.class);	
+	
+	when(projeto11.getDescricao()).thenReturn("Métodos de Otimização");
+	when(projeto12.getDescricao()).thenReturn("Uso eficaz de Métricas");
+	when(projeto21.getDescricao()).thenReturn("Achmus");
+	when(projeto22.getDescricao()).thenReturn("Arquimedes");
+	
+	LinhaPesquisa linha1 = mock(LinhaPesquisa.class);
+	LinhaPesquisa linha2 = mock(LinhaPesquisa.class);	
+	
+	when(linha1.getName()).thenReturn("Métodos Ágeis");
+	when(linha2.getName()).thenReturn("Software Livre");
+	
+	projetos1 = new ArrayList<Projeto>(2);
+	projetos2 = new ArrayList<Projeto>(2);
+	
+	projetos1.add(projeto11);
+	projetos1.add(projeto12);
+	
+	projetos2.add(projeto21);
+	projetos2.add(projeto22);
+	
+	when(linha1.getProjetos()).thenReturn(projetos1);
+	when(linha2.getProjetos()).thenReturn(projetos2);	
 	
 	pesquisas = new ArrayList<LinhaPesquisa>(2);
-	pesquisas.add(new LinhaPesquisa("Sistemas"));
-	pesquisas.add(new LinhaPesquisa("Bancos de Dados"));
+	pesquisas.add(linha1);
+	pesquisas.add(linha2);	
 	
-	grupo.setPesquisas(pesquisas);	
+	grupo = new Grupo();
+	grupo.setNome("Grupo de Sistemas de Software");
+	grupo.setPesquisas(pesquisas);
+
 	return grupo;	
     }
 }
