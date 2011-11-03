fec48ecd2f002f8f5cd704ef8c9e37730b29c0d5
Jorge Leandro <jleandro@vision.ime.usp.br>
2011-11-03 15:29:03 -0200

    Implementacao do Teste de unidade para a classe LinhaDePesquisaControllerTest

---
+++ src/sigp/test/controllers/LinhaDePesquisaControllerTest.java
@@ -0,0 +1,70 @@
+package sigp.test.controllers;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import sigp.src.LinhaPesquisa;
+import sigp.src.Projeto;
+import sigp.src.controllers.LinhaDePesquisaController;
+import sigp.src.dao.LinhaDePesquisaDao;
+import br.com.caelum.vraptor.Result;
+
+public class LinhaDePesquisaControllerTest {
+    
+        LinhaDePesquisaController controller;
+	Result result;
+	LinhaDePesquisaDao dao;
+	List<LinhaPesquisa> list;
+	List<Projeto> projetos;
+
+    @Before
+    public void setUp() throws Exception {
+	result = mock(Result.class);
+	dao = mock(LinhaDePesquisaDao.class);
+	controller = new LinhaDePesquisaController(result, dao);
+	
+	LinhaDePesquisaController controlmock = mock(LinhaDePesquisaController.class);
+	when(result.redirectTo(LinhaDePesquisaController.class)).thenReturn(controlmock);
+	
+	setUpDao();
+    }
+
+    private void setUpDao() {	
+	list = new ArrayList<LinhaPesquisa>();
+	{
+		LinhaPesquisa l = new LinhaPesquisa();
+		LinhaPesquisa subl = new LinhaPesquisa();
+		l.setIdPesquisa(1L);
+		l.setNome("Pesquisa 1");		
+		l.setProjetos(projetos);
+		l.setSubLinha(subl);
+		list.add(l);
+	}
+	when(dao.list()).thenReturn(list);
+	when(dao.getLinhaPesquisa(1L)).thenReturn(list.get(0));	
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	controller = null;
+	result = null;
+	dao = null;
+    }
+
+    @Test
+    public void testIndex() {
+	controller.index();
+	verify(result).include("linhasDePesquisa", list);
+    }
+
+}
