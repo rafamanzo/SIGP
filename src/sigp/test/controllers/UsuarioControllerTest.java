55e3fb0c81ab2407e0de21a5a53b88d9b94932bb
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2011-11-07 17:43:04 -0200

    Arrumei uns testes.

--- src/sigp/test/controllers/UsuarioControllerTest.java
+++ src/sigp/test/controllers/UsuarioControllerTest.java
@@ -25,86 +25,88 @@ import sigp.src.dao.UsuarioDao;
 import br.com.caelum.vraptor.Result;
 
 public class UsuarioControllerTest {
-    
+
     UsuarioController controller;
     Result result;
-    UsuarioDao dao;    
-    
+    UsuarioDao dao;
+
     File avatar;
     TipoUsuario tipoUsuario;
     String senha;
     String login;
     Contribuinte contribuinte;
-    
+
     List<Usuario> usuarios;
 
     @Before
     public void setUp() throws Exception {
-	result = mock(Result.class);
-	dao = mock(UsuarioDao.class);
-	controller = new UsuarioController(result, dao);
+        result = mock(Result.class);
+        dao = mock(UsuarioDao.class);
+        controller = new UsuarioController(result, dao);
 
-	UsuarioController controlmock = mock(UsuarioController.class);
-	when(result.redirectTo(UsuarioController.class)).thenReturn(controlmock);
-	setUpDao();
+        UsuarioController controlmock = mock(UsuarioController.class);
+        when(result.redirectTo(UsuarioController.class))
+                .thenReturn(controlmock);
+        setUpDao();
     }
-    
+
     private void setUpDao() {
-	usuarios = new ArrayList<Usuario>();
-	{
-	    Usuario usuario = new Usuario();	    
-	    Contribuinte contribuinte = mock(Contribuinte.class);    
-	    usuario.setContribuinte(contribuinte);
-	    
-	    usuario.setLogin("mgerosa");
-	    usuario.setSenha("engsoft");
+        usuarios = new ArrayList<Usuario>();
+        {
+            Usuario usuario = new Usuario();
+            Contribuinte contribuinte = mock(Contribuinte.class);
+            usuario.setContribuinte(contribuinte);
 
-	    usuarios.add(usuario);
-	}
-	
-	{
-	    Usuario usuario = new Usuario();	    
-	    Contribuinte contribuinte = mock(Contribuinte.class);    
-	    usuario.setContribuinte(contribuinte);
-	    
-	    usuario.setLogin("jleandro");
-	    usuario.setSenha("vision");
+            usuario.setLogin("mgerosa");
+            usuario.setSenha("engsoft");
 
-	    usuarios.add(usuario);
-	}
-	
-	when(dao.list()).thenReturn(usuarios);
-	when(dao.pegaUsuarioPorLogin("mgerosa")).thenReturn(usuarios.get(0));
-	when(dao.pegaUsuarioPorLogin("jleandro")).thenReturn(usuarios.get(1));	
+            usuarios.add(usuario);
+        }
+
+        {
+            Usuario usuario = new Usuario();
+            Contribuinte contribuinte = mock(Contribuinte.class);
+            usuario.setContribuinte(contribuinte);
+
+            usuario.setLogin("jleandro");
+            usuario.setSenha("vision");
+
+            usuarios.add(usuario);
+        }
+
+        when(dao.list()).thenReturn(usuarios);
+        when(dao.pegaUsuarioPorLogin("mgerosa")).thenReturn(usuarios.get(0));
+        when(dao.pegaUsuarioPorLogin("jleandro")).thenReturn(usuarios.get(1));
     }
 
     @After
     public void tearDown() throws Exception {
-	controller = null;
-	result = null;
-	dao = null;
-	usuarios = null;
+        controller = null;
+        result = null;
+        dao = null;
+        usuarios = null;
     }
-    
+
     @Test
-    public void testVerifica(){	
-	controller.verifica("ninguem", "nada");
-	verify(result).include("usuarioNaoCadastrado", "Usuario nao cadastrado.");
+    public void testVerifica() {
+        controller.verifica("ninguem", "nada");
+        verify(result).include("usuarioNaoCadastrado",
+                "Usuario nao cadastrado.");
     }
 
     @Test
     public void testSalva() {
-	//Testar tentativa de persistencia de usuario ja existente
-	Usuario u = usuarios.get(0);
-	controller.salva(u);
-	verify(result).include("usuarioJaExistente", "Login nao disponivel.");
-	
-	//Testar insercao persistencia de novo usuario
-	Usuario unovo = new Usuario();
-	unovo.setLogin("newnuser");
-	unovo.setSenha("newpssd");
-	controller.salva(unovo);
-	verify(dao).save(unovo);	
+        // Testar tentativa de persistencia de usuario ja existente
+        Usuario u = usuarios.get(0);
+        controller.salva(u, "engsoft");
+        verify(result).include("usuarioJaExistente", "Login nao disponivel.");
+
+        // Testar insercao persistencia de novo usuario
+        Usuario unovo = new Usuario();
+        unovo.setLogin("newnuser");
+        unovo.setSenha("newpssd");
+        controller.salva(unovo, "newpssd");
+        verify(dao).save(unovo);
     }
 
 }
