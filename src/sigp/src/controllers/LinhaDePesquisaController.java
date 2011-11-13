31f4066a72bd2e7984e6be4fa65bc9852eec8e97
Jorge Leandro <jleandro@vision.ime.usp.br>
2011-11-13 03:58:21 -0200

    LinhaPesquisa: tentativas de passar lista de parametros para controler

--- src/sigp/src/controllers/LinhaDePesquisaController.java
+++ src/sigp/src/controllers/LinhaDePesquisaController.java
@@ -1,9 +1,12 @@
 package sigp.src.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sigp.src.LinhaPesquisa;
+import sigp.src.Projeto;
 import sigp.src.dao.LinhaDePesquisaDao;
+import sigp.src.dao.ProjetoDao;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
@@ -13,17 +16,19 @@ import br.com.caelum.vraptor.Validator;
 public class LinhaDePesquisaController {
     private final Result result;
     private final LinhaDePesquisaDao dao;
+    private final ProjetoDao pdao;
     private Validator validator;
 
-    public LinhaDePesquisaController(Result result, Validator validator, LinhaDePesquisaDao dao) {
+    public LinhaDePesquisaController(Result result, Validator validator, LinhaDePesquisaDao dao, ProjetoDao pdao) {
         this.result = result;
         this.validator = validator;
         this.dao = dao;
+        this.pdao = pdao;
     }
 
     @Path("/linhadepesquisa/")
     public void index() {
-        result.include("linhasdepesquisa", dao.list());
+        result.include("linhasdepesquisa", dao.list());        
     }
 
     public List<LinhaPesquisa> lista() {
@@ -33,12 +38,28 @@ public class LinhaDePesquisaController {
     @Path("/linhadepesquisa/novo")
     public void novalinhadepesquisa() {
         result.include("linhasdepesquisa", dao.list());
+        result.include("todosprojetos", pdao.list());
     }
 
     @Path("/linhadepesquisa/inserir")
-    public void inserir(final LinhaPesquisa linhapesquisa) {
-        validator.validate(linhapesquisa);
-        validator.onErrorForwardTo(this).novalinhadepesquisa();
+   // public void inserir(final LinhaPesquisa linhapesquisa, final List<String> idprojetos) {
+    public void inserir(final LinhaPesquisa linhapesquisa){
+    	//Funciona! Ver:
+    	//http://www.guj.com.br/java/235513-select-multiple-no-vraptor-312-
+//    	List<Projeto> projetos = new ArrayList<Projeto>();
+//    	for (int i=0; i<idprojetos.size();i++)
+//    		projetos.add(pdao.getProjeto(Long.parseLong(idprojetos.get(i))));
+//    	
+
+    	//'Pau' no Hibernate ao 'setar' a lista de projetos para linhapesquisa:
+    	//Could not execute JDBC batch update
+    	//Duplicate entry 'id_do_objeto' for key 'LINHAP_ID'
+//    	linhapesquisa.setProjetos(projetos);
+
+
+    	
+      //  validator.validate(linhapesquisa);
+      //  validator.onErrorForwardTo(this).novalinhadepesquisa();
         dao.save(linhapesquisa);
         result.redirectTo(this).index();
     }
@@ -50,14 +71,15 @@ public class LinhaDePesquisaController {
             result.redirectTo(this).index();
         else
             result.include("linhapesquisa", linhapesquisa);
-        result.include("linhasdepesquisa", dao.list());
+        result.include("linhasdepesquisa", dao.list());  
+        result.include("todosprojetos", pdao.list());    
     }
 
     @Path("/linhadepesquisa/altera")
     public void altera(final LinhaPesquisa linhapesquisa, final Long subLinha) {
         validator.validate(linhapesquisa);
         validator.onErrorForwardTo(this).editar(linhapesquisa.getIdPesquisa());
-        linhapesquisa.setSubLinha(dao.getLinhaPesquisa(subLinha));
+        linhapesquisa.setSubLinha(dao.getLinhaPesquisa(subLinha));        
         dao.update(linhapesquisa);
         result.redirectTo(this).index();
     }
